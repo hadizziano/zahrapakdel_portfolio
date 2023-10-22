481e4a77b382fec626321a264a1ddcf42d636e6e
hadizzo <masimo.hadizzo@gmail.com>
2023-10-22 13:05:56 +0330

    it is completed

--- src/pages/digitalarts/list.js
+++ src/pages/digitalarts/list.js
@@ -1,8 +1,26 @@
 export const paintingList = [
   {
-    Aname: "Digital Arts",
-    Technique: "Chiaroscuro",
-    url: "./pictures/chris.jpg",
+    Aname: "The wolf",
+    Technique: "digital pen",
+    url: "./digital/wolf.jpg",
+    description: "This is an artwork",
+  },
+  {
+    Aname: "The leopard",
+    Technique: "digital pen",
+    url: "./digital/leopard.jpg",
+    description: "This is an artwork",
+  },
+  {
+    Aname: "The elephant",
+    Technique: "digital pen",
+    url: "./digital/elephant.jpg",
+    description: "This is an artwork",
+  },
+  {
+    Aname: "Mountains",
+    Technique: "digital pen",
+    url: "./digital/mountains.jpg",
     description: "This is an artwork",
   },
 ];
